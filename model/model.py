78eb6233765b786118037ecb9bed8c1298bfe3e6
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-08-22 23:40:38 +0900

    Feat : `model.ipynb`, `model.py`
    - python 파일 수정
    - ipynb 코랩에서 작동되도록 수정

--- model/model.py
+++ model/model.py
@@ -1,49 +1,119 @@
-from keras.layers import Convolution2D, ZeroPadding2D, Activation, MaxPooling2D, Dropout, Flatten
+from keras.models import Sequential
 from sklearn.model_selection import train_test_split
-train_x, test_x, train_y, test_y = train_test_split(features, target_classes, test_size=0.30)
+from PIL import Image
 
-#VGG-Face model
-model = Sequential()
-model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
-model.add(Convolution2D(64, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2)))
+import os
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
 
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(128, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(128, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2)))
+from torchvision import datasets
+import glob
 
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2)))
+from keras.layers import Input, Conv2D, Activation, MaxPool2D, Dropout, Flatten, Dense
+from keras.optimizers import Adam
+from keras.models import Model
 
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2)))
+def imshow(inp, title=None):
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)
 
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
-model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-model.add(Convolution2D(4096, (7, 7), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Convolution2D(4096, (1, 1), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Convolution2D(2622, (1, 1)))
-model.add(Flatten())
-model.add(Activation('softmax'))
+def fileCategoriesAppend():
+    categories = []
+
+    for i in range(1, 101):
+        categories.append(f"{i}")
+
+    print(categories)
+    return categories
+
+
+def fileLoad(categories):
+    data_dir = "./data"
+    num_classes = len(categories)
+
+    image_w = 224
+    image_h = 224
+
+    pixels = image_w * image_h * 3
+
+    x = []
+    y = []
+
+    for idx, category in enumerate(categories):
+        label = [0 for i in range(num_classes)]
+        label[idx] = 1
+
+        image_dir = data_dir + "/" + category
+        files = glob.glob(image_dir + "/*.jpg")
+
+        for i,f in enumerate(files):
+            img = Image.open(f)
+            img = img.convert("RGB")
+            img = img.resize((image_w, image_h))
+            data = np.asarray(img)
+
+            x.append(data)
+            y.append(label)
+
+            if i % 700 == 0:
+                print(category, " : ", f)
+
+    x = np.array(x)
+    y = np.array(y)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y)
+    return x_train, y_train
+
+
+def modelGenerate():
+    input = Input(shape=(224, 224, 3))
+
+    cnn1 = Conv2D(128, kernel_size=3, activation='relu')(input)
+    cnn1 = Conv2D(128, kernel_size=3, activation='relu')(cnn1)
+    cnn1 = Conv2D(128, kernel_size=3, activation='relu')(cnn1)
+    cnn1 = MaxPool2D(pool_size=3, strides=2)(cnn1)
+
+    cnn2 = Conv2D(128, kernel_size=3, activation='relu')(cnn1)
+    cnn2 = Conv2D(128, kernel_size=3, activation='relu')(cnn2)
+    cnn2 = Conv2D(128, kernel_size=3, activation='relu')(cnn2)
+    cnn2 = MaxPool2D(pool_size=3, strides=2)(cnn2)
+
+    cnn3 = Conv2D(256, kernel_size=3, activation='relu')(cnn2)
+    cnn3 = Conv2D(256, kernel_size=3, activation='relu')(cnn3)
+    cnn3 = Conv2D(256, kernel_size=3, activation='relu')(cnn3)
+    cnn3 = MaxPool2D(pool_size=3, strides=2)(cnn3)
+
+    cnn4 = Conv2D(512, kernel_size=3, activation='relu')(cnn3)
+    cnn4 = Conv2D(512, kernel_size=3, activation='relu')(cnn4)
+    cnn4 = Conv2D(512, kernel_size=3, activation='relu')(cnn4)
+    cnn4 = MaxPool2D(pool_size=3, strides=2)(cnn4)
+
+    dense = Flatten()(cnn4)
+    dense = Dropout(0.2)(dense)
+    dense = Dense(1024, activation='relu')(dense)
+    dense = Dense(1024, activation='relu')(dense)
+
+    output = Dense(1, activation='linear', name='age')(dense)
+
+    model = Model(input, output)
+    model.compile(optimizer=Adam(0.0001), loss='mse', metrics=['mae'])
+
+    model.summary()
+    return model
+
+def modelFit(x_train, y_train, model):
+    model.fit(x_train, y_train, batch_size=30, epochs=5000)
+
+x_train, y_train = fileLoad(fileCategoriesAppend())
+modelFit(x_train, y_train, modelGenerate())
